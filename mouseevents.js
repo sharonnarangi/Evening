30a8ec49c7722544ae9365854a4c61ad53187667
Sharon Nrangi <sharonnarangi@gmail.com>
2024-10-03 13:26:40 +0300

    Adding Event Listeners

---
+++ mouseevents.js
@@ -0,0 +1,6 @@
+//An event listener listens for specific events to create interactive web pages
+//events:Clicks,mouse hover
+//addEventListener(event,callback)
+const myBox = document.getElementById("myBox");
+function changeColour
+myBox.addEventListener('click')
