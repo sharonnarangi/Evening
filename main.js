800e17bbddb3240a97b4660c173c0fd606560a74
Sharon Nrangi <sharonnarangi@gmail.com>
2024-11-24 16:51:15 +0300

    Server Creation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-const authFunctions = require("./auth.js");
+const authFunctions = require("./auth.js");//The other word for "Require" is "Import"
 
 authFunctions.checkPassword();
+
+//The HTTP module. Once I install node I get access to HTTP module.
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Hello World!!!')
+//Import or require module.
+const http = requier("http")//This is an object. Almost everything in JavaScript is an boject
+
+http.createServer((request,response)=>{
+    console.log(request.headers);
+    //process incoming request
+    
+    response.end("Hello,this is your server responding to a request")
+})
+
+//I have built my server there!
+//To start my server----Tell my computer to listen to incoming request through a certain port.
+//PORT-----We have hardware ports and we have software ports-----numbers assigned to applications for identity as they
+//communicate with other devices.
+http.listen(3000, ()=>console.log("server started and running on port 3000"))
